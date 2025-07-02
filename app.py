ef7f893c8cdd0be182d105994300b09c7175b6fd
MrMap-code111 <mrmap0104@gmail.com>
2025-07-02 15:12:13 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -111,9 +111,5 @@ def logout():
     session.pop('user', None)
     return redirect(url_for('login'))
 
-# Khởi động ứng dụng
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 10000))  # 10000 là dự phòng khi chạy local
-    app.run(host='0.0.0.0', port=port)
+
 
